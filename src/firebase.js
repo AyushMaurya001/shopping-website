654e37be856c9a429d53a21d48fc4711cd81116a
Ayush <120236359+AyushMaurya001@users.noreply.github.com>
2024-04-02 13:12:44 +0530

    make changes to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_apiKey,
-  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
-  projectId: import.meta.env.VITE_FIREBASE_projectId,
-  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
-  appId: import.meta.env.VITE_FIREBASE_appId
+  apiKey: String(import.meta.env.VITE_FIREBASE_apiKey),
+  authDomain: String(import.meta.env.VITE_FIREBASE_authDomain),
+  projectId: String(import.meta.env.VITE_FIREBASE_projectId),
+  storageBucket: String(import.meta.env.VITE_FIREBASE_storageBucket),
+  messagingSenderId: String(import.meta.env.VITE_FIREBASE_messagingSenderId),
+  appId: String(import.meta.env.VITE_FIREBASE_appId)
 };
 
 const firebase = initializeApp(firebaseConfig);
